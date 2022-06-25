615a7784e94dc0c0e7829bfa36a6e31b8f1cef51
Andres Huelmo <huelmoandres@gmail.com>
2022-06-25 15:26:47 -0300

    Fix port error.

--- src/config/app.js
+++ src/config/app.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 module.exports = {
     api: {
         basePath: process.env.API_BASE_PATH || '/v1',
-        port: process.env.API_PORT || 3001,
+        port: process.env.PORT || 3001,
     },
 }
