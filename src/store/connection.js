7c1d66fa747650974c528c2f9bd1c5a2cbba62fb
Andres Huelmo <huelmoandres@gmail.com>
2022-06-25 14:50:58 -0300

    Sequelize to heroku

--- src/store/connection.js
+++ src/store/connection.js
@@ -6,9 +6,13 @@ let sequelize = null;
 
 module.exports = function setupDatabase() {
     if (!sequelize) {
-        sequelize = new Sequelize(config.database, config.username, config.password, {
-            host: config.host,
-            dialect: config.dialect
+        sequelize = new Sequelize(process.env.DATABASE_URL, {
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            }
         });
     }
     return sequelize;
