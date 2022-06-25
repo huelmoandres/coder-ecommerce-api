283386297720e7c0dedda61ebf6e984d386a4a2b
Andres Huelmo <huelmoandres@gmail.com>
2022-06-25 16:05:37 -0300

    Add public assets

--- src/api/index.js
+++ src/api/index.js
@@ -10,6 +10,8 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
+console.log(__dirname);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(api.basePath + '/category', networkCategory);
