6fbf2fa4a11ddcbc3bb28e737cb943ce5a999c34
Andres Huelmo <huelmoandres@gmail.com>
2022-06-25 14:58:47 -0300

    added heroku config info

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,14 @@
 module.exports = {
     connections: {
         postgres: {
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
-            dialect: process.env.DB_CONNECTION,
-            logging: process.env.NODE_ENV === 'production' ? false : console.log
-        }
+            "use_env_variable": "DATABASE_URL",
+            "dialect": "postgres",
+            "dialectOptions": {
+                "ssl": {
+                    "require": true,
+                    "rejectUnauthorized": false
+                }
+            }
+        },
     }
 }
--- src/database/config.js
+++ src/database/config.js
@@ -2,4 +2,4 @@ require('dotenv').config();
 
 const {connections} = require("../config/database");
 
-module.exports = connections.mysql;
+module.exports = connections.postgres;
